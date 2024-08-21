b5ee1627542c3276a924ecfc979f1faf9cc3e763
Anonymous <anonymous@example.com>
2024-08-22 00:21:19 +0200

    changed url

--- frontend/base-path.mjs
+++ frontend/base-path.mjs
@@ -1,2 +1,2 @@
-export const BASE_PATH_WITHOUT_DOMAIN = '/better-kickbase-web';
-export const SITE = 'https://felixschusi.github.io';
+export const BASE_PATH_WITHOUT_DOMAIN = '/kickbase-information';
+export const SITE = 'https://senoramarillo.github.io';
