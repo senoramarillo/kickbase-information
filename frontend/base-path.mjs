a1e771e79158a2eca3438fd085e6d3ef425a804a
Anonymous <anonymous@example.com>
2024-08-25 09:06:44 +0200

    fixed bundesliga table

--- frontend/base-path.mjs
+++ frontend/base-path.mjs
@@ -1,2 +1,13 @@
 export const BASE_PATH_WITHOUT_DOMAIN = '/kickbase-information';
 export const SITE = 'https://senoramarillo.github.io';
+
+export const BUNDESLIGATABLE_API_CONFIG  = {
+    BASE_URL: 'https://api.better-kickbase.workers.dev/competition/table',
+    DEFAULT_OPTS: {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+    }
+};
--- frontend/src/services/bundesliga-table.service.ts
+++ frontend/src/services/bundesliga-table.service.ts
@@ -1,44 +1,35 @@
-import { BundesligaTable, BundesligaTableEntry } from '../models/bundesliga-table';
+import {BundesligaTable, BundesligaTableEntry, bundesligaTableEntryFromApiResponse} from '../models/bundesliga-table';
+import {BUNDESLIGATABLE_API_CONFIG} from '../../base-path.mjs';
 
 export class BundesligaTableService {
-  public async getData(): Promise<BundesligaTable> {
-    // The path `/competition/table` of the kickbase api doesnt work anymore.
-    // This should be fixed but for now we just mock the response.
-    return this.mockData();
-  }
 
-  private mockData(): BundesligaTable {
-    const teams: BundesligaTableEntry[] = [
-      { goadDifference: 1, matches: 1, points: 3, place: 5, teamId: '43', teamName: 'Leipzig', trend: 0 },
-      { goadDifference: 0, matches: 1, points: 1, place: 7, teamId: '10', teamName: 'Bremen', trend: 0 },
-      { goadDifference: 0, matches: 1, points: 1, place: 6, teamId: '13', teamName: 'Augsburg', trend: 0 },
-      { goadDifference: 0, matches: 0, points: 0, place: 12, teamId: '39', teamName: 'St. Pauli', trend: 0 },
-      { goadDifference: 0, matches: 1, points: 1, place: 9, teamId: '18', teamName: 'Mainz 05', trend: 0 },
-      { goadDifference: 0, matches: 0, points: 0, place: 18, teamId: '4', teamName: 'Frankfurt', trend: 0 },
-      { goadDifference: 0, matches: 0, points: 0, place: 17, teamId: '9', teamName: 'Stuttgart', trend: 0 },
-      { goadDifference: 0, matches: 0, points: 0, place: 14, teamId: '51', teamName: 'Kiel', trend: 0 },
-      { goadDifference: 0, matches: 0, points: 0, place: 15, teamId: '15', teamName: 'M-Gladbach', trend: 0 },
-      { goadDifference: 1, matches: 1, points: 3, place: 3, teamId: '14', teamName: 'Hoffenheim', trend: 0 },
-      { goadDifference: 0, matches: 1, points: 1, place: 8, teamId: '40', teamName: 'Union', trend: 0 },
-      { goadDifference: 0, matches: 0, points: 0, place: 16, teamId: '24', teamName: 'Bochum', trend: 0 },
-      { goadDifference: 1, matches: 1, points: 3, place: 4, teamId: '7', teamName: 'Bayer 04', trend: 0 },
-      { goadDifference: 2, matches: 1, points: 3, place: 1, teamId: '5', teamName: 'Freiburg', trend: 0 },
-      { goadDifference: 0, matches: 0, points: 0, place: 13, teamId: '11', teamName: 'Wolfsburg', trend: 0 },
-      { goadDifference: 0, matches: 0, points: 0, place: 11, teamId: '2', teamName: 'Bayern', trend: 0 },
-      { goadDifference: 2, matches: 1, points: 3, place: 2, teamId: '3', teamName: 'Dortmund', trend: 0 },
-      { goadDifference: 0, matches: 0, points: 0, place: 10, teamId: '50', teamName: 'Heidenheim', trend: 0 }
-      // { goadDifference: 0, matches: 0, points: 0, place: 16, teamId: '20', teamName: 'Hertha', trend: 0 }
-      // { goadDifference: 0, matches: 0, points: 0, place: 7, teamId: '28', teamName: 'Köln', trend: 0 },
-      // { goadDifference: 0, matches: 0, points: 0, place: 18, teamId: '8', teamName: 'Schalke', trend: 0 },
-      // { goadDifference: 0, matches: 0, points: 0, place: 18, teamId: '42', teamName: 'Darmstadt', trend: 0 },
-    ];
-    const bundesligaTable: BundesligaTable = {
-      matchDay: 1,
-      currentMatchDay: 1,
-      teams
-    };
-    return bundesligaTable;
-  }
+    private readonly BASE_URL: string = BUNDESLIGATABLE_API_CONFIG.BASE_URL;
+    private readonly defaultOpts: RequestInit = BUNDESLIGATABLE_API_CONFIG.DEFAULT_OPTS;
+
+    public async getData(): Promise<BundesligaTable> {
+        try {
+            const response = await fetch(this.BASE_URL, this.defaultOpts);
+            if (!response.ok) {
+                throw new Error(`Failed to fetch data: ${response.statusText}`);
+            }
+
+            const rawTableData: any = await response.json();
+            return this.transformApiResponse(rawTableData);
+
+        } catch (error) {
+            console.error('Error fetching Bundesliga table data:', error);
+            throw error;
+        }
+    }
+
+    private transformApiResponse(rawTableData: any): BundesligaTable {
+        const teams: BundesligaTableEntry[] = rawTableData.t.map(bundesligaTableEntryFromApiResponse);
+        return {
+            matchDay: rawTableData.md,
+            currentMatchDay: rawTableData.cmd,
+            teams
+        };
+    }
 }
 
 export const bundesligaTableService = new BundesligaTableService();
