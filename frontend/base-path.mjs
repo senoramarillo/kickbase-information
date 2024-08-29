33e0763e758c95cb70dc6577e4b77722493b7de0
Anonymous <anonymous@example.com>
2024-08-29 16:06:59 +0200

    added notes

--- frontend/base-path.mjs
+++ frontend/base-path.mjs
@@ -1,5 +1,6 @@
 export const BASE_PATH_WITHOUT_DOMAIN = '/kickbase-information';
 export const SITE = 'https://senoramarillo.github.io';
+// https://api.kickbase.com/competition/best?position=0 - Top 25 players
 
 export const BUNDESLIGATABLE_API_CONFIG  = {
     BASE_URL: 'https://api.better-kickbase.workers.dev/competition/table',
